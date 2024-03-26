2eb19ccd3f266abb39e0e94f89c7c3f816f00d0e
fededav <67575679+raishid@users.noreply.github.com>
2024-03-26 01:05:49 -0400

    update readme.md

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# PyChatango
+
+Send messages to Chatango rooms automatically
+
+## Install
+pip install pychatango-dev
+
+
+## Usage
+
+```
+  from pyChatango import pyChatango
+  
+  ctango = pyChatango('chatango_url', 'username', 'password')
+  ctango.chat('Hello World!!!')
+```
+
+
+
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="pyChatango",
-    version="0.1",
+    name="pychatango-dev",
+    version="0.1.1",
     packages=find_packages(),
     description="Send messages to Chatango chatrooms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Raishid",
     author_email="raishidavid@gmail.com",
     url="https://github.com/raishid/pyChatango",
